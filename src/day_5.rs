3168e82672ba3a5487dffd47b65f01271a409196
Julien <julien@canevas.eu>
2022-12-05 20:51:12 +0100

    Working first question

--- src/day_5.rs
+++ src/day_5.rs
@@ -2,12 +2,21 @@
 fn do_instr(instr: &str, yard: &mut Vec<Vec<char>>) {
     let instrs: Vec<&str> = instr.split(' ').collect();
     let amount: usize = instrs[1].parse().unwrap();
-    let src: usize = instrs[3].parse().unwrap();
-    let dst: usize = instrs[5].parse().unwrap();
+    let mut src: usize = instrs[3].parse().unwrap();
+    let mut dst: usize = instrs[5].parse().unwrap();
+    src -= 1;
+    dst -= 1;
+    // println!("Move {amount} crate from stack {src} to stack {dst}.");
+    // println!("Source stack before instruction {:?}", yard[src]);
+    // println!("Dest stack before instruction {:?}", yard[dst]);
     for _ in 0..amount {
-        let b_id = yard[src].pop().unwrap();
-        yard[dst].push(b_id);
+        match yard[src].pop() {
+            Some(b_id) => yard[dst].push(b_id),
+            None => (),
+        }
     }
+    // println!("Source stack after instruction {:?}", yard[src]);
+    // println!("Dest stack after instruction {:?}", yard[dst]);
 }
 
 /// Initialize the yard state and get the list of instructions
@@ -15,33 +24,35 @@ fn parse(content: &str) -> (Vec<Vec<char>>, &str) {
     let splt: Vec<&str> = content.split("\n\n").collect();
     let yard_str = splt[0];
     let instr_str = splt[1];
-    // get amount of columns and consume only one element
-    // of the iterator after reverse. Number of chars: n_cols * 4 - 1
-    let mut yard_lines = yard_str.lines().rev();
-    let n_col: usize = yard_lines
-        .next()
-        .unwrap()
-        .split(' ')
+    // get number of columns by getting the last element
+    // of the latest line.
+    let mut yard_lines = yard_str.lines();
+    let n_col = yard_lines
         .next_back()
         .unwrap()
+        .chars()
+        .rev()
+        .find(|&x| x.is_numeric())
+        .unwrap()
+        .to_string()
         .parse()
         .unwrap();
     let n_chars_line = n_col * 4 - 1;
     let mut yard = vec![Vec::new(); n_col];
-    // re-reverse the iterator, then on each line,
+    // reverse the iterator to parse in the correct order,
+    // then on each line,
     // move by a certain amount depending on the character we get
-    // Alternatively, could use the advance_by nightly feature
+    // Alternatively, could use the next_by iterator
     for line in yard_lines.rev() {
         let mut idx = 0;
-        let chs : Vec<char> = line.chars().collect();
-        while idx < n_chars_line{
+        let chs: Vec<char> = line.chars().collect();
+        while idx < n_chars_line {
             let ch = chs[idx];
-            if  ch == ' '{
+            if ch == ' ' {
                 idx += 4
-            }
-            else{
-                let b_id = line.chars().nth(idx).unwrap();
-                yard[idx/4].push(b_id);
+            } else {
+                let b_id = line.chars().nth(idx + 1).unwrap();
+                yard[idx / 4].push(b_id);
                 idx += 4
             }
         }
@@ -49,26 +60,24 @@ fn parse(content: &str) -> (Vec<Vec<char>>, &str) {
     (yard, instr_str)
 }
 
-pub fn solve_day_first_question(content : &str){
+pub fn solve_day_first_question(content: &str) {
     let (mut yard, instrs) = parse(content);
-    for instr in instrs.lines(){
+    for instr in instrs.lines() {
         do_instr(instr, &mut yard)
     }
-    // Rev all piles as we parsed from top to bottom, and get top id
-    for s in yard.iter_mut(){
-        s.reverse();
-        println!("{}",s.pop().unwrap())
+    for s in yard.iter_mut() {
+        print!("{}", s.pop().unwrap())
     }
 }
 
-pub fn solve_day_second_question(content : &str){
+pub fn solve_day_second_question(content: &str) {
     let (mut yard, instrs) = parse(content);
-    for instr in instrs.lines(){
+    for instr in instrs.lines() {
         do_instr(instr, &mut yard)
     }
     // Rev all piles as we parsed from top to bottom, and get top id
-    for s in yard.iter_mut(){
+    for s in yard.iter_mut() {
         s.reverse();
-        println!("{}",s.pop().unwrap())
+        print!("{}", s.pop().unwrap())
     }
 }
